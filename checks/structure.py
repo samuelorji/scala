8c692c85dd46c099a8b18a2d82a757d6af2cea34
Szymon Rodziewicz <szymonrodant@gmail.com>
2020-10-07 17:19:53 +0200

    Structure checks

---
+++ checks/structure.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+
+@dataclass
+class Course:
+  id: str
+  name: str
+  levels: [str]
+  image: str
+  video: str
+  desc: str
+  language: str
+  scope: [str]
+
+@dataclass
+class TopicRange:
+  topic_id: str
+  lesson_start: str
+  lesson_end: str
+
+@dataclass
+class Level:
+  level: str
+  course_id: str
+  name: str
+  desc: str
+  ranges: [TopicRange]
+
+@dataclass 
+class Lesson:
+  id: str
+  topic_id: str
+  title: str
+  author_id: str
+  duration: int
+
+@dataclass 
+class Topic:
+  id: str
+  name: str
+  desc: str
+  lessons: [Lesson]
+
+@dataclass
+class Author:
+  id: str
+  name: str
+  order: int
+  twitter: str
+  github: str
+  desc: str
+
+@dataclass
+class Structure:
+  courses: [Course]
+  levels: [Level]
+  topics: [Topic]
+  authors: [Author]
---
+++ checks/test_topic.py
@@ -0,0 +1,19 @@
+from structure_loader import *
+
+STRUCTURE = load_structure()
+TOPICS = STRUCTURE.topics
+
+def test_lesson_unique():
+  for topic in TOPICS:
+    lessons = [lesson.id for lesson in topic.lessons]
+    # it's n^2 but we won't be having more than 20 lessons in topic at this moment
+    duplicates = list({lesson for lesson in lessons if lessons.count(lesson) > 1}) 
+    for duplicate in duplicates:
+      assert False, f"Lesson {duplicate} is duplicated in topic {topic.id}"
+  
+def test_prerequisites():
+  lessonToCourse 
+  for topic in TOPICS:
+    for lesson in topic.lessons:
+      
+      
