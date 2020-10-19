44cdd8db253fbef96244283692f01b7bf7cd258c
Szymon Rodziewicz <szymonrodant@gmail.com>
2020-10-19 13:30:08 +0200

    Companies & authors update

--- .github/workflows/deployToGcs.sh
+++ .github/workflows/deployToGcs.sh
@@ -8,11 +8,11 @@ find ./ -type f -exec sed -i -e 's/&quot;/"/g' {} \;
 
 gcloud info
 DIRS=(pages courses topics contexts icons courseImages contentImages)
-FILES=(revision authors.json)
+FILES=(revision authors.json companies.json)
 gsutil rm -r "gs://$CONTENT_BUCKET/*"
-for dir in ${DIRS[@]};do
+for dir in "${DIRS[@]}"; do
     gsutil cp -r "$dir" "gs://$CONTENT_BUCKET"
 done
-for file in ${FILES[@]}; do
+for file in "${FILES[@]}"; do
     gsutil cp "$file" "gs://$CONTENT_BUCKET"
 done
